3ee41b10e7911a23def68c2be1bfd24d61734124
Acer <Acer@169.254.186.170>
2021-03-06 23:13:51 +0200

    css added, new structure improoved

--- src/index.js
+++ src/index.js
@@ -1,27 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 
-import AppHeader from './components/app-header';
-import SearchPanel from "./components/search-panel";
-import TodoList from "./components/todo-list";
-
-const App = () => {
-
-    const todoData = [
-        {label: "Drimk Coffee", important: false, id: 1},
-        {label: "Make awesome  App", important: true, id: 2},
-        {label: "Have a lunch", important: false, id: 3},
-    ]
-    return (
-        <div>
-            <AppHeader />
-            <SearchPanel />
-            <TodoList todos = {todoData}/>
-        </div>
-    );
-}
-
-
+import App from './components/app'
 
 
 ReactDom.render(<App />, document.getElementById('root'));
